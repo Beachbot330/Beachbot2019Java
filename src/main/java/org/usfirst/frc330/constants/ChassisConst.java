ff191db1d3f469b9c487d7d0a48c162ae9547fa6
elijaholmos <eli.olmos9@gmail.com>
2019-02-02 12:03:14 -0800

    added encoder, low, and high gearRatio constants

--- src/main/java/org/usfirst/frc330/constants/ChassisConst.java
+++ src/main/java/org/usfirst/frc330/constants/ChassisConst.java
@@ -13,8 +13,9 @@ public final class ChassisConst {
     //Encoder Distance Constants
     public static final double  wheelDiameter        = 6.0;       //ejo 2.2.19
     public static final double  pulsePerRevolution   = 1024;
-    public static final int     encoderGearRatio     = 0;       //TODO - Get Correct Value from Shane - AP
-    public static final int     gearRatio            = 0;       //TODO - Get Correct Value from Shane - AP
+    public static final double  encoderGearRatio     = 36/12;   //ejo 2.2.19 (supplied by TD)
+    public static final double  highGearRatio        = 8.75/1;  //ejo 2.2.19 (supplied by TD)
+    public static final double  lowGearRatio         = 18/1;    //ejo 2.2.19 (supplied by TD)
     public static final double  Fudgefactor          = 0;       //TODO - Find the 2019 value - AP
 
     //Turn Gyro
