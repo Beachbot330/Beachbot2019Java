251dd8dd97a308b738c3afd50c1377010e9895fe
elijaholmos <eli.olmos9@gmail.com>
2019-02-02 11:40:40 -0800

    updated lift constants

--- src/main/java/org/usfirst/frc330/constants/LiftConst.java
+++ src/main/java/org/usfirst/frc330/constants/LiftConst.java
@@ -14,7 +14,7 @@ public final class LiftConst {
 	public static final double DeployBallMid	= 18.5;		//ejo 2.2.19
 	public static final double DeployBallHi		= 30.75;	//ejo 2.2.19 (might be changed)
 
-	public static final double DeployHatchLow	= 0.0;		//TODO: Get Value from Shane -AP
+	public static final double DeployHatchLow	= 0.0;		//ejo 2.2.19
 	public static final double DeployHatchMid	= 13.5;		//ejo 2.2.19
 	public static final double DeployHatchHi	= 27.5;		//ejo 2.2.19
 
@@ -23,10 +23,10 @@ public final class LiftConst {
 	public static final double tolerance		= 0; //TODO: find 2019 value -AP
 
 	//Encoder Distance Constants
-    public static final double  wheelDiameter        = 1.751;       //TODO - Get Correct Value from Shane - EJO
+    public static final double  wheelDiameter        = 1.751;   //inches ejo 2.2.19
     public static final double  pulsePerRevolution   = 1024;
     public static final int     encoderGearRatio     = 0;       //TODO - Get Correct Value from Shane - EJO
-    public static final int     gearRatio            = 0;       //TODO - Get Correct Value from Shane - EJO
+    public static final int     gearRatio            = 1;       //ejo 2.2.19
     public static final double  Fudgefactor          = 0;       //TODO - Find the 2019 value - ejo
 
 	//encoder is on the output shaft, so it should be gearRation should be 1:1
