f08f48f93d88b1f379efe9ab74f9fdbcb18b199d
Allen Peters <git@allenpeters.com>
2019-02-01 22:10:05 -0800

    Start implementing defense

--- src/main/java/org/usfirst/frc330/constants/LiftConst.java
+++ src/main/java/org/usfirst/frc330/constants/LiftConst.java
@@ -6,4 +6,17 @@ public final class LiftConst {
 	
 	private LiftConst(){}
 
+	public static final double defense	= 0.0;		//TODO: Confirm with Shane -AP
+
+	public static final double DeployBallLow	= 0.0;		//TODO: Get Value from Shane -AP
+	public static final double DeployBallMid	= 0.0;		//TODO: Get Value from Shane -AP
+	public static final double DeployBallHi		= 0.0;		//TODO: Get Value from Shane -AP
+
+	public static final double DeployHatchLow	= 0.0;		//TODO: Get Value from Shane -AP
+	public static final double DeployHatchMid	= 0.0;		//TODO: Get Value from Shane -AP
+	public static final double DeployHatchHi	= 0.0;		//TODO: Get Value from Shane -AP
+
+	public static final double tolerance = 0; //TODO: find 2019 value -AP
+
+
 }	
