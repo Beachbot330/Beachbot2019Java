84faab1c54de884085b212b25b89b0ca1eb68a34
Allen Peters <git@allenpeters.com>
2019-02-01 21:51:55 -0800

    Add back in chassis constants that we needed

--- src/main/java/org/usfirst/frc330/constants/ChassisConst.java
+++ src/main/java/org/usfirst/frc330/constants/ChassisConst.java
@@ -7,6 +7,18 @@ package org.usfirst.frc330.constants;
 import org.usfirst.frc330.wpilibj.PIDGains;
 
 public final class ChassisConst {
-	private ChassisConst(){}
+
+    private ChassisConst(){}
+
+    //Encoder Distance Constants
+    public static final double  wheelDiameter        = 0;       //TODO - Get Correct Value from Shane - AP
+    public static final double  pulsePerRevolution   = 1024;
+    public static final int     encoderGearRatio     = 0;       //TODO - Get Correct Value from Shane - AP
+    public static final int     gearRatio            = 0;       //TODO - Get Correct Value from Shane - AP
+    public static final double  Fudgefactor          = 0;       //TODO - Find the 2019 value - AP
+
+    //Turn Gyro
+	public static final int     gyroTolerancebuffer = 0;        //TODO - Find the 2019 value - AP
+    public static final double  gyroTurnMin         = 0;        //TODO - Find the 2019 value - AP
        
 }
