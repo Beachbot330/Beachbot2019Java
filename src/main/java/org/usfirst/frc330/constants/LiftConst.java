1b0c71affa6abe33289f076ee90a465875733763
trailblazer84562 <git@allenpeters.com>
2019-02-02 14:07:58 -0800

    Port the majority of Lift subsystem from 2018

--- src/main/java/org/usfirst/frc330/constants/LiftConst.java
+++ src/main/java/org/usfirst/frc330/constants/LiftConst.java
@@ -6,9 +6,10 @@ public final class LiftConst {
 	
 	private LiftConst(){}
 
+	//Positions
 	public static final double defense			= 0.0;		//ejo 2.2.19
-	public static final double LowHardStop 		= 0.0;		//ejo 2.2.19
-	public static final double HighHardStop 	= 30.75;	//ejo 2.2.19
+	public static final double lowerLimit 		= 0.0;		//ejo 2.2.19
+	public static final double upperLimit	 	= 30.75;	//ejo 2.2.19
 
 	public static final double DeployBallLow	= 5.0;		//ejo 2.2.19
 	public static final double DeployBallMid	= 18.5;		//ejo 2.2.19
@@ -23,12 +24,35 @@ public final class LiftConst {
 	public static final double tolerance		= 0; //TODO: find 2019 value -AP
 
 	//Encoder Distance Constants
-    public static final double  pulsePerRevolution   = 1024;
-    public static final double  encoderGearRatio     = 1/1;     //ejo 2.2.19
-    public static final double 	gearRatio            = 1.751;    //ejo 2.2.19
-    public static final double  Fudgefactor          = 0;       //TODO - Find the 2019 value - ejo
-
+	public static final double  ticksPerRev   		= 4096;	//AP 02/02/2019
+	public static final double  inchesPerRev		= 5.50093; //AP 02/02/2019
+    public static final double  encoderGearRatio    = 1/1;     //ejo 2.2.19
+    public static final double 	sprocketDiameter    = 1.751;    //ejo 2.2.19
+	public static final double  Fudgefactor         = 0;       //TODO - Find the 2019 value - ejo
 	//encoder is on the output shaft, so it should be gearRation should be 1:1
+	
+	//Talon SRX Settings
+	public static final double VoltageRampRate				= 1.23;	// AP 2018 value
+	public static final double MaxOutputPercent				= 0.3;	// AP Testing Value
+	public static final int    CAN_Timeout					= 10; // AP 2018 value
+	public static final int    CAN_Timeout_No_Wait      	= 0;  // AP 2018 value
+	public static final int    CAN_Status_Frame_13_Period 	= 20; // AP 2018 value
+	public static final int    CAN_Status_Frame_10_Period 	= 20; // AP 2018 value
+
+	// PID Constants
+	public static final double proportional      	= 0.3;   	// AP 2018 value
+	public static final double integral         	= 0.000; 	// AP 2018 value
+	public static final double derivative        	= 15.0;   	// AP 2018 value
+	public static final double feedforward          = 0.5;   	// AP 2018 value
+
+	// Motion Magic Constants
+	public static final int velocityLimit              = 2000;   // AP 2018 value
+	public static final int accelLimit                 = 6000;   // AP 2018 value
+
+	// Other
+	public static final double calibrationSpeed			= -0.15; // AP 2018 value
+
+
 
 
 }	
