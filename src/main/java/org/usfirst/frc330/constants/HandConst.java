82db8dc4fa76c01f16bc20546e6dbf3c68073272
trailblazer84562 <git@allenpeters.com>
2019-02-10 14:18:09 -0800

    Implement HandOut

--- src/main/java/org/usfirst/frc330/constants/HandConst.java
+++ src/main/java/org/usfirst/frc330/constants/HandConst.java
@@ -13,13 +13,16 @@ public final class HandConst {
 	//angles relative to the ground (degrees)
 	public static final double defense 			= 57.0; 	//ejo 2.2.19
 	public static final double hardStop 		= 57.0;		//ejo 2.2.19
-	public static final double hatchPlacement 	= 0.0; 		//ejo 2.2.19
+	public static final double hatchPlacementLow 	= 0.0; 		//ejo 2.2.19
+	public static final double hatchPlacementMid 	= 0.0; 		//AP 2/10/19
+	public static final double hatchPlacementHigh 	= 0.0; 		//AP 2/10/19
+	public static final double ballPlacementLow 	= 0.0; 		//AP 2/10/19
+	public static final double ballPlacementMid 	= 0.0; 		//AP 2/10/19
+	public static final double ballPlacementHigh 	= 35.0; 	//ejo 2.2.19
 	public static final double hatchPickup	 	= -2.0; 		//AP 2/9/19
 	public static final double postHatchPickup	= 0.0; 		//AP 2/9/19
-	
 	public static final double ballPickup 		= -46.0;	//ejo 2.2.19
 
-	public static final double ballPlacementHi 	= 35.0;		//ejo 2.2.19
 	
 	
 	
