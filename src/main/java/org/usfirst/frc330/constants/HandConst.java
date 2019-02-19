8e3c52a7fea187971ffd9e6ed1fc0eec82735e86
Joe <sciencewhiz@users.noreply.github.com>
2019-02-18 19:38:48 -0800

    More Hand updates. Manual control and limits working.

--- src/main/java/org/usfirst/frc330/constants/HandConst.java
+++ src/main/java/org/usfirst/frc330/constants/HandConst.java
@@ -21,7 +21,21 @@ public final class HandConst {
 	public static final double postHatchPickup	= 0.0; 		//AP 2/9/19
 	public static final double ballPickup 		= -46.0;	//ejo 2.2.19
 
+	//TODO temporary use versaplanetary. Real robot will be 1.0;
+	public static final double encoderGearRatio = 66.0/18.0;	  // JR Practice Robot 2/18/19
+
+	public static final int    CAN_Timeout					= 10; // AP 2018 value
+	public static final int    CAN_Timeout_No_Wait      	= 0;  // AP 2018 value
+
+	public static final double VoltageRampRate				= 12;	// AP 2018 value
+	public static final double MaxOutputPercent				= 0.3;	// AP Testing Value
+
 	
+	// Motion Magic Constants
+	public static final int velocityLimit              = 2000;   // AP 2018 value
+	public static final int accelLimit                 = 6000;   // AP 2018 value
+
+	public static final double GamepadDeadZone			= 0.1; //AP 2/17/19
 	
 	
 
