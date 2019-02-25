63e852829b5fde0834b95885c3b39a8c4431a736
Joe <sciencewhiz@users.noreply.github.com>
2019-02-24 16:58:35 -0800

    Clean up DoNothing command

--- src/main/java/org/usfirst/frc330/autoCommands/DoNothing.java
+++ src/main/java/org/usfirst/frc330/autoCommands/DoNothing.java
@@ -2,7 +2,6 @@ package org.usfirst.frc330.autoCommands;
 
 import org.usfirst.frc330.commands.*;
 import edu.wpi.first.wpilibj.command.BBCommandGroup;
-import edu.wpi.first.wpilibj.command.WaitCommand;
 
 /**
  *
@@ -13,8 +12,6 @@ public class DoNothing extends BBCommandGroup {
     public DoNothing() {
     	
        addSequential(new IsFinishedFalse());
-       addParallel(new IsFinishedFalse());
-       addParallel(new WaitCommand(1.0));
        
     }
 }
