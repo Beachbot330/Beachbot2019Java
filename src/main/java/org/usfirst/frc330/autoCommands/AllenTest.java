019bdef0c8f2472faec272edf962699529f5ed38
trailblazer84562 <git@allenpeters.com>
2019-03-10 17:46:30 -0700

    Tuning drive train. Add hand velocity logging

--- src/main/java/org/usfirst/frc330/autoCommands/AllenTest.java
+++ src/main/java/org/usfirst/frc330/autoCommands/AllenTest.java
@@ -3,6 +3,8 @@ package org.usfirst.frc330.autoCommands;
 
 import org.usfirst.frc330.commands.*;
 import org.usfirst.frc330.commands.commandgroups.Defense;
+import org.usfirst.frc330.commands.drivecommands.DriveDistance;
+import org.usfirst.frc330.constants.ChassisConst;
 
 import edu.wpi.first.wpilibj.command.BBCommandGroup;
 
@@ -14,8 +16,9 @@ public class AllenTest extends BBCommandGroup {
 
     public AllenTest() {
     	
-    	addSequential(new ShiftHigh());
-    	addSequential(new Defense());
+    	addSequential(new ShiftLow());
+        //addSequential(new Defense());
+        addSequential(new DriveDistance(221.5, ChassisConst.DriveLow));
     	
     }
 }
